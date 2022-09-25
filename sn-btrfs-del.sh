ad9f7b973ad0679f98eecc98faa9f954870b70bc
snarkrans <snarkrans@gmail.com>
2022-09-25 18:39:08 +0200

    Update sn-btrfs-del.sh

--- sn-btrfs-del.sh
+++ sn-btrfs-del.sh
@@ -1,9 +1,6 @@
-count_all=$(sudo btrfs subv list / | grep -E "/home.|/root." | wc -l)
-count_left=30
-sudo btrfs subv list / | grep -E "/home.|/root." | head -n+$(($count_all -$count_left ))| cut -d ' ' -f 9 | xargs -i echo /{} | xargs -i sudo btrfs subvolume delete {}
+all=$(sudo btrfs subv list / | grep -E "/home.|/root." | wc -l)
+save=30
+sudo btrfs subv list / | grep -E "/home.|/root." | head -n+$(($all -$save ))| cut -d ' ' -f 9 | xargs -i echo /{} | xargs -i sudo btrfs subvolume delete {}
 exit
 
 Скрипт удалит все старые снимки, тое сть все снимки кроме 30 последних. 
-
-
-
