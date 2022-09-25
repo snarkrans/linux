4163f4d70a823dfbe0c92138d675c5ba5bb795d0
snarkrans <snarkrans@gmail.com>
2022-09-25 18:31:01 +0200

    Update sn-btrfs-snapshots.sh

--- sn-btrfs-snapshots.sh
+++ sn-btrfs-snapshots.sh
@@ -7,7 +7,7 @@ count_left=30
 sudo btrfs subv list / | grep -E "/home.|/root." | head -n+$(($count_all -$count_left ))| cut -d ' ' -f 9 | xargs -i echo /{} | xargs -i sudo btrfs subvolume delete {}
 Exit
 
-Скрипт создает снимки сабвольюмов /@ и /@home в директорию /.snapshots и удаляет старые снимки. 30 последних снимков не будут удалены. 
+Скрипт создает снимки сабвольюмов /@ и /@home в директорию /.snapshots и удаляет все снимки кроме 30 последних.
 Скрипт можно добавить в crontab командой sudo EDITOR=vim crontab -e. Команда 0 4 * * * /home/user/sn-btrfs-snapshots.sh будет запускать скрипт каждый день в 04:00 часа.
 Число хранимых снимков можно изменить отредактировав переменную count_left.
 Отправить снимки по ssh можно командой sudo btrfs -v send /.snapshots/* | sudo ssh -p 22 root@192.168.43.72 "btrfs receive /.snapshots"
