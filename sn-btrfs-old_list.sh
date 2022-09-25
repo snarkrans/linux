b5d4fcf4a1a9658d3813e0b1b5da315efb5d532e
snarkrans <snarkrans@gmail.com>
2022-09-25 18:39:34 +0200

    Update sn-btrfs-old_list.sh

--- sn-btrfs-old_list.sh
+++ sn-btrfs-old_list.sh
@@ -1,9 +1,7 @@
-count_all=$(sudo btrfs subv list / | grep -E "/home.|/root." | wc -l)
-count_left=30
+all=$(sudo btrfs subv list / | grep -E "/home.|/root." | wc -l)
+save=30
 echo old snapshots 
-sudo btrfs subv list / | grep -E "/home.|/root." | head -n+$(($count_all -$count_left ))| cut -d ' ' -f 9 | xargs -i echo /{} | xargs -i echo {}
+sudo btrfs subv list / | grep -E "/home.|/root." | head -n+$(($all -$save ))| cut -d ' ' -f 9 | xargs -i echo /{} | xargs -i echo {}
 exit
 
 Скрипт покажет старые снимки, то есть все снимки кроме 30 последних. 
-
-
