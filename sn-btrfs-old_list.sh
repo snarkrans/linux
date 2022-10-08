e20d006d6820004f0120f651ff5f24f8546ebc10
snarkrans <snarkrans@gmail.com>
2022-10-08 16:09:54 +0200

    Delete sn-btrfs-old_list.sh

--- sn-btrfs-old_list.sh
+++
@@ -1,7 +0,0 @@
-all=$(sudo btrfs subv list / | grep -E "/home.|/root." | wc -l)
-save=30
-echo old snapshots 
-sudo btrfs subv list / | grep -E "/home.|/root." | head -n+$(($all -$save ))| cut -d ' ' -f 9 | xargs -i echo /{} | xargs -i echo {}
-exit
-
-Скрипт покажет старые снимки, то есть все снимки кроме 30 последних. 
