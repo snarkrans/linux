91a7d49d7ea1608d6d087d4418955ec7f82a52cf
snarkrans <snarkrans@gmail.com>
2022-10-13 15:27:53 +0200

    Update blenderupdate.sh

--- blenderupdate.sh
+++ blenderupdate.sh
@@ -11,5 +11,5 @@ cd ~/blender/downloads/alpha/
 var=$(wget -qO- https://builder.blender.org/download/daily/ | grep -o -G 'blender-[^a-z]*-alpha+master.[0-9a-z]*-linux.x86_64-release.tar.xz' | awk 'NR==1')
 axel -n 10 https://builder.blender.org/download/daily/$var
 tar -xf ~/blender/downloads/alpha/*.tar.xz -C ~/blender/downloads/alpha
-cp ~/blender/BlenderAlpha.desktop ~/.local/share/applications
+#cp ~/blender/BlenderAlpha.desktop ~/.local/share/applications
 #sed -ie 's|Exec.*|Exec=~/blender/downloads/alpha/blender-*/blenderSSSSSSSSSSS %f |g' BlenderAlpha.desktop
