eddfe913784cf4539ee30a1b4c33e918a2b1e12f
snarkrans <snarkrans@github.com>
2023-10-20 14:39:14 +0200

    test

---
+++ config/nixos/configuration.nix
@@ -0,0 +1,187 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # from Alex
+  nixpkgs.config = {
+  allowUnfree = true;
+  };
+  nixpkgs.overlays = [
+  (self: super: { fcitx-engines = self.fcitx5; })
+  ];
+  nix = { extraOptions = "experimental-features = flakes"; };
+  location = {latitude = 0.0; longitude = 100.0; };
+
+  hardware = {
+  opengl = {
+  enable = true;
+  extraPackages = with pkgs; [vaapiIntel intel-media-driver vaapiVdpau libvdpau-va-gl];
+  };
+  cpu.intel.updateMicrocode = true;
+  enableAllFirmware = true;
+  enableRedistributableFirmware = true;
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "t470"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Rome";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.snark = {
+    isNormalUser = true;
+    description = "denis";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    firefox
+    ];
+  };
+
+  programs.zsh.enable = true;
+  users.users.snark.shell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh ];
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "snark";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  wget 
+  neovim 
+  neofetch 
+  syncthing
+  pass
+  pinentry-curses
+  blueman
+  pavucontrol
+  xfce.xfce4-xkb-plugin 
+  xfce.xfce4-pulseaudio-plugin
+  ];
+
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  services.blueman.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+  enable = true;
+  # require public key authentication for better security
+  settings.PasswordAuthentication = false;
+  settings.KbdInteractiveAuthentication = false;
+  #settings.PermitRootLogin = "yes";
+  };
+
+  # Syncthing is a continuous file synchronization program.
+  services = {
+  syncthing = {
+  enable = true;
+  user = "snark";
+  dataDir = "/home/snark/Documents";    # Default folder for new synced folders
+  configDir = "/home/snark/.config/syncthing";   # Folder for Syncthing's settings and keys
+  };
+  };
+ 
+  # Gnupg agent for Pass.
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+  enable = true;
+  pinentryFlavor = "curses";
+  enableSSHSupport = true;
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
