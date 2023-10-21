e6d555a51ceba7198e5eb1d6e09d515af9c9362e
snarkrans <snarkrans@github.com>
2023-10-21 09:36:18 +0200

    test

--- config/nixos/configuration.nix
+++ config/nixos/configuration.nix
@@ -1,15 +1,12 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
+# Help is available in the configuration.nix(5) man page
 { config, pkgs, ... }:
 
 {
   imports =
-    [
-      ./hardware-configuration.nix
-      ./packages.nix
-    ];
+  [
+  ./hardware-configuration.nix
+  ./packages.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -18,14 +15,16 @@
   networking.hostName = "t470"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
   # Set your time zone.
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
   time.timeZone = "Europe/Rome";
 
   # Select internationalisation properties.
@@ -38,10 +37,14 @@
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
 
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "snark";
+  
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+  layout = "us";
+  xkbVariant = "";
   };
 
   # Enable sound with pipewire.
@@ -49,16 +52,16 @@
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  # use the example session manager (no others are packaged yet so this is enabled by default,
+  # no need to redefine it in your config for now)
+  #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -66,26 +69,23 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.snark = {
-    isNormalUser = true;
-    description = "denis";
-    extraGroups = [ "networkmanager" "wheel" ];
+  isNormalUser = true;
+  description = "denis";
+  extraGroups = [ "networkmanager" "wheel" ];
   # packages = with pkgs; [firefox];
   };
-
+  
+  # Enable zsh.
   programs.zsh.enable = true;
   users.users.snark.shell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
 
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "snark";
-
-  # started in user sessions.
-    programs.gnupg.agent = {
-    enable = true;
-    pinentryFlavor = "curses";
-    enableSSHSupport = true;
-    };
+  # Started in user sessions.
+  programs.gnupg.agent = {
+  enable = true;
+  pinentryFlavor = "curses";
+  enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
@@ -110,11 +110,7 @@
   };
   };
  
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
 
   system.stateVersion = "23.05"; # Did you read the comment?
 }
